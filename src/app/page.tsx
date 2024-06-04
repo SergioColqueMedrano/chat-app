0f32296aec0f5142e7c0593a2a19888d65ada6fe
“tyraiel” <tyraiel@gmail.com>
2024-06-04 10:43:15 -0300

    themes blue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  return <p>Hello World!</p>
+  return <Button>Hello World!</Button>
 }
